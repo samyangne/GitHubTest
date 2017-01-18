5522335d1a33cb304e291fda0fa80b44fe76f784
samyangne <1129993231@qq.com>
2017-01-18 13:50:22 +0800

    this is version 2

---
+++ src/cn/tarena/Test.java
@@ -0,0 +1,10 @@
+package cn.tarena;
+
+/**
+ * Created by Administrator on 2017/1/18 0018.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("this is test 2");
+    }
+}
