15896523445e5b20ad604bebb26aac2f9f8d0937
samyangne <1129993231@qq.com>
2017-01-18 13:44:02 +0800

    this is version 1

---
+++ src/cn/tarena/GitHubTest.java
@@ -0,0 +1,11 @@
+package cn.tarena;
+
+/**
+ * Created by Administrator on 2017/1/18 0018.
+ */
+public class GitHubTest {
+
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+    }
+}
